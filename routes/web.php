a63e586dce5005429c714ae69f1569e8bf8d643e
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-16 10:57:38 -0400

    [add]: Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Assinatura base de uma route
+// Route::verb('url', callback); O call back é a ação que vai ser executada quando a rota for acionada
+
+// Rotas
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+// Rota com função anônima
+Route::get('/rota', function() {
+    return '<h1>Olá Laravel</h1>';
+});
+
+Route::get('/user', function() {
+    return '<h1>O user está aqui</h1>';
+});
+
+Route::get('/injection', function(Request $request) {
+    var_dump($request);
+});
+
+// O match aceita get e post simultaneamente
+Route::match(['get', 'post'], '/match', function() {
+    return '<h1>Aceita GET e POST</h1>';
+});
+
+Route::any('/any', function() {
+    return '<h1>Aceita qualquer http verb</h1>';
+});
+
+// Routes com Controller
+Route::get('/index', [MainController::class, 'index']);
+Route::get('/about', [MainController::class, 'about']);
+
+Route::redirect('/saltar', 'index');
+Route::permanentRedirect('/saltar2', 'index');
+
+Route::view('/view', 'welcome');
+Route::view('/view2', 'welcome', ['myName' => "Gustavo Barbosa"]);
