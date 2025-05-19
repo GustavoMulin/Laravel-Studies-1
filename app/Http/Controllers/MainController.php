24c36c9e37987f442617b70f50f10f7d7e098eae
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-19 09:11:50 -0400

    [add]: Route Parameters

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,4 +15,34 @@ class MainController extends Controller
     {
         echo 'about';
     }
+
+    public function mostrarValor($valor)
+    {
+        echo "Valor enviado pela rota: $valor";
+    }
+
+    public function mostrarValores($valor1, $valor2)
+    {
+        echo "Valores enviados pela rota: $valor1 é $valor2";
+    }
+
+    public function mostrarValores2(Request $request, $valor1, $valor2)
+    {
+        echo "Valores enviados pela rota: $valor1 é $valor2";
+    }
+
+    public function mostrarValorOpcional($valor = null)
+    {
+        echo "Valor opcional: $valor";
+    }
+
+    public function mostrarValorOpcional2($valor1, $valor2 = null)
+    {
+        echo "Valor opcional: $valor1 e $valor2";
+    }
+
+    public function mostrarPosts($user_id, $post_id)
+    {
+        echo "Post do usuário ID: $user_id e o post com ID: $post_id";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,15 @@ Route::permanentRedirect('/saltar2', 'index');
 
 Route::view('/view', 'welcome');
 Route::view('/view2', 'welcome', ['myName' => "Gustavo Barbosa"]);
+
+// --------------------------------------
+// ROUTE PARAMETERS
+// --------------------------------------
+
+Route::get('/valor/{value}', [MainController::class, 'mostrarValor']);
+Route::get('/valores/{value1}/{value2}', [MainController::class, 'mostrarValores']);
+Route::get('/valores2/{value1}/{value2}', [MainController::class, 'mostrarValores2']);
+
+Route::get('/opcional/{value?}', [MainController::class, 'mostrarValorOpcional']);
+Route::get('/opcional2/{value1}/{value2?}', [MainController::class, 'mostrarValorOpcional2']);
+Route::get('/user/{user_id}/post/{post_id}', [MainController::class, 'mostrarPosts']);
