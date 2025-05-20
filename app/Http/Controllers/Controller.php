1f78de8fd83f11879819a0266df5cf37e7b9a041
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-20 10:36:54 -0400

    [add]: Controller criado pelo laravel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function cleanUpperCasseString($string): string
+    {
+        // Remover os espaços em branco no ínico e fim de uma string
+        // converte a string para uppercase (maiúscula)
+        return strtoupper(trim($string));
+    }
 }
