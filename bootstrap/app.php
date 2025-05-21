3bbbfd0b241748acd13c7cf7d1da38c8739ecb00
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-21 09:13:06 -0400

    [add]: Definir Middleware Globais

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\EndMiddleware;
+use App\Http\Middleware\StartMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,26 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        
+        // // Adiconar a todas as rotas
+        // $middleware->prepend([
+        //     StartMiddleware::class,
+        // ]);
+
+        // // Adicionar no fianl de todas as respostar de todas as rotas
+        // $middleware->append([
+        //     EndMiddleware::class
+        // ]);
+
+        // Criar grupo de middleware
+        $middleware->prependToGroup('inicio',  [
+            StartMiddleware::class
+        ]);
+
+        $middleware->appendToGroup('fim',  [
+            EndMiddleware::class
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
