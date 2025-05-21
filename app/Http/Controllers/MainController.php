0a8a0664a94def470e588d76297151c2ab0bf00f
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-21 10:16:42 -0400

    [add]: Passar Dados do Controller Para a View

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,6 +9,34 @@ class MainController extends Controller
 {
     public function showView(): view
     {
-        return view('home');
+        // Método 1
+        /*
+        $data = [
+            'name' => 'Gustavo',
+            'idade' => '21'
+        ];
+        return view('home', $data);
+        */
+
+        // Método 2
+        /*
+        return view('home', [
+            'name' => 'Gustavo',
+            'idade' => '21'
+        ]);
+        */
+
+        // Método 3
+        /*
+        return view('home')
+                ->with('name', "Gustavo")
+                ->with('idade', "21");
+        */
+
+        // Método 4
+        $name = "Gustavo";
+        $idade = "21";
+
+        return view('home', compact('name', 'idade'));
     }
 }
